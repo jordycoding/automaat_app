ac45d454fea15687c1c864b6aee7e3c3bc1cb855
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2025-01-21 17:08:27 +0100

    Login screen

---
+++ lib/config/constants.dart
@@ -0,0 +1,3 @@
+class AppConstants {
+  static const String serverUrl = "http://localhost:8080/api";
+}
---
+++ lib/ui/login/widgets/login_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Text("Automaat",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headlineMedium),
+              ),
+              SizedBox(height: 24),
+              TextFormField(
+                  decoration: const InputDecoration(
+                      border: UnderlineInputBorder(), labelText: "Username"),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter a username";
+                    }
+                    return null;
+                  }),
+              TextFormField(
+                decoration: const InputDecoration(
+                    border: UnderlineInputBorder(), labelText: "Password"),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a password";
+                  }
+                  return null;
+                },
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FilledButton(onPressed: () {}, child: const Text("Login")),
+                  SizedBox(width: 10),
+                  OutlinedButton(
+                      onPressed: () {}, child: const Text("Register"))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
