9442e6f5cc3bdfb3bdf2a7d2d4aa3df1680066f6
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2025-01-24 17:22:27 +0100

    Extract http logic to mixin

--- lib/data/services/api/auth_api_client.dart
+++ lib/data/services/api/auth_api_client.dart
@@ -5,10 +5,11 @@ import 'package:automaat_app/config/constants.dart';
 import 'package:automaat_app/data/services/api/model/login_request/login_request.dart';
 import 'package:automaat_app/data/services/api/model/login_response/login_response.dart';
 import 'package:automaat_app/data/services/api/model/register_request/register_request.dart';
+import 'package:automaat_app/utils/http_delegate.dart';
 import 'package:automaat_app/utils/result.dart';
 import 'package:logging/logging.dart';
 
-class AuthApiClient {
+class AuthApiClient with HttpDelegate {
   AuthApiClient({HttpClient Function()? clientFactory})
       : _clientFactory = clientFactory ?? HttpClient.new;
 
@@ -16,44 +17,12 @@ class AuthApiClient {
   final _log = Logger("AuthApiClient");
 
   Future<Result<void>> register(RegisterRequest registerRequest) async {
-    final client = _clientFactory();
-    try {
-      final request = await client
-          .postUrl(Uri.parse("${AppConstants.serverUrl}/AM/register"));
-      request.headers.contentType = ContentType.json;
-      request.write(jsonEncode(registerRequest));
-      final response = await request.close();
-      if (response.statusCode == 201) {
-        return Result.ok(null);
-      } else {
-        return Result.error(Exception("Failed to register"));
-      }
-    } on Exception catch (error) {
-      return Result.error(error);
-    } finally {
-      client.close();
-    }
+    return postRequest(Uri.parse("${AppConstants.serverUrl}/AM/register"),
+        _clientFactory, registerRequest);
   }
 
   Future<Result<LoginResponse>> login(LoginRequest loginRequest) async {
-    final client = _clientFactory();
-    try {
-      final request = await client
-          .postUrl(Uri.parse("${AppConstants.serverUrl}/authenticate"));
-      request.headers.contentType = ContentType.json;
-      request.write(jsonEncode(loginRequest));
-      final response = await request.close();
-      if (response.statusCode == 200) {
-        final stringData = await response.transform(utf8.decoder).join();
-        return Result.ok(LoginResponse.fromJson(jsonDecode(stringData)));
-        _log.info(stringData);
-      } else {
-        return const Result.error(HttpException("Login error"));
-      }
-    } on Exception catch (error) {
-      return Result.error(error);
-    } finally {
-      client.close();
-    }
+    return postRequest(Uri.parse("${AppConstants.serverUrl}/authenticate"),
+        _clientFactory, loginRequest, LoginResponse.fromJson);
   }
 }
---
+++ lib/utils/http_delegate.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:automaat_app/utils/result.dart';
+
+mixin HttpDelegate {
+  Future<Result<T?>> getRequest<T>(Uri uri, HttpClient Function() clientFactory,
+      [T Function(Map<String, Object?>)? fromJson]) async {
+    final client = clientFactory();
+    try {
+      final request = await client.getUrl(uri);
+      final response = await request.close();
+      if (response.statusCode >= 200 && response.statusCode <= 300) {
+        if (T == dynamic) {
+          return Result.ok(null);
+        } else {
+          final stringData = await response.transform(utf8.decoder).join();
+          if (fromJson == null) {
+            throw Exception("Please provide fromjson for parsing");
+          }
+          return Result.ok(fromJson(jsonDecode(stringData)));
+        }
+      } else {
+        return Result.error(Exception("Error fetching $uri"));
+      }
+    } on Exception catch (error) {
+      return Result.error(error);
+    } finally {
+      client.close();
+    }
+  }
+
+  Future<Result<T>> postRequest<T>(
+      Uri uri, HttpClient Function() clientFactory, Object body,
+      [T Function(Map<String, Object?>)? fromJson]) async {
+    final client = clientFactory();
+    try {
+      final request = await client.postUrl(uri);
+      request.headers.contentType = ContentType.json;
+      request.write(jsonEncode(body));
+      final response = await request.close();
+      if (response.statusCode >= 200 && response.statusCode <= 300) {
+        if (T == dynamic) {
+          return Result.ok(null as T);
+        } else {
+          final stringData = await response.transform(utf8.decoder).join();
+          if (fromJson == null) {
+            throw Exception("Please provide fromjson for parsing");
+          }
+          return Result.ok(fromJson(jsonDecode(stringData)));
+        }
+      } else {
+        return Result.error(Exception("Error posting to $uri"));
+      }
+    } on Exception catch (error) {
+      return Result.error(error);
+    } finally {
+      client.close();
+    }
+  }
+}
