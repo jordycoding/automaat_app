9c18331180a4939d64d48870a28404b5dbf3af2a
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2025-01-26 20:49:08 +0100

    Push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,57 @@
 import 'package:automaat_app/config/dependencies.dart';
 import 'package:automaat_app/data/repositories/car_list/car_repository.dart';
 import 'package:automaat_app/data/services/api/api_client.dart';
+import 'package:automaat_app/firebase_options.dart';
 import 'package:automaat_app/routing/app_routes.dart';
 import 'package:automaat_app/routing/router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  String? token = await messaging.getToken();
+  print("Device token: $token");
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+      _showSnackbar(message.notification!.title ?? "No title",
+          message.notification!.body ?? "No body");
+    }
+  });
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  print("User granted permission: ${settings.authorizationStatus}");
   runApp(
     MultiProvider(
       providers: [
-        ...providers, 
+        ...providers,
         Provider(
           create: (context) => CarRepository(context.read<ApiClient>()),
         ),
@@ -18,6 +59,19 @@ void main() {
       child: const MyApp(),
     ),
   );
+}
+
+void _showSnackbar(String title, String body) {
+  final context = rootNavigatorKey.currentContext;
+
+  if (context != null) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$title: $body'),
+        duration: Duration(seconds: 3),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
